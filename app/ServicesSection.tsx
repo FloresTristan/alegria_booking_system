b8c4f09749e8f2ad624c9f3eced17bd1256f7a02
FloresTristan <michaeltristanflores646@gmail.com>
2026-01-18 00:21:08 +0800

    -- added the floating section divider and footer --
    updated some the ui elements to follow the mockup designs, also attached the icons and logos used from the mockup

--- app/ServicesSection.tsx
+++ app/ServicesSection.tsx
@@ -2,14 +2,14 @@
 export default function ServicesSection(){
     return (
         <div
-            className="flex h-screen items-center justify-center font-poppins"
+            className="flex h-screen w-full items-center justify-center font-poppins"
         >
-            <div className="flex justify-center bg-red-50 h-50 w-50">
-                <p
-                    className="text-3xl font-bold text-[#75C308]"
+            <div className="flex justify-center bg-red-50 h-50 w-[50%]">
+                <h1
+                    className="text-[#75C308] font-black text-7xl "
                 >
                     Alegria, Cebu
-                </p>
+                </h1>
             </div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,39 @@
 import Image from "next/image";
 import ServicesSection from "./ServicesSection";
+import Navbar from "@/components/Navbar";
+import FloatingSectionDivider from "@/components/FloatingSectionDivider";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans flex-col">
+    <div className="flex min-h-screen  items-center justify-center bg-zinc-50 font-sans flex-col">
+      <div
+        className="w-full h-[10%] z-50"
+      >
+        <Navbar />
+      </div>
       {/* first section of hero page */}
       <div
-        className="h-screen "
+        className="h-[90%] "
       >
         <Image
             className=""
             src="/alegria.png"
-            alt="Next.js logo"
-            fill
+            alt="Alegria Background"
+            width={1920}
+            height={1080}
             priority
           />
       </div>
 
+      <div
+        className="w-full h-16"
+      >
+        <FloatingSectionDivider />
+      </div>
+
       <ServicesSection />
+      <Footer />
     </div>
   );
 }
