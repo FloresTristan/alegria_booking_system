4dd6f9c3e4fd15d144d322aef46c87ef3c44f71e
FloresTristan <michaeltristanflores646@gmail.com>
2026-01-19 14:00:13 +0800

    -- created Gallery section --
    unfinished gallery cards

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import ServicesSection from "./ServicesSection";
 import Navbar from "@/components/Navbar";
 import FloatingSectionDivider from "@/components/FloatingSectionDivider";
+import GallerySection from "./GallerySection";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -33,6 +34,8 @@ export default function Home() {
       </div>
 
       <ServicesSection />
+      <GallerySection />
+
       <Footer />
     </div>
   );
